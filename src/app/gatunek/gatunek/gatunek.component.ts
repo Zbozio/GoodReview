e181ab58c7be8f5e5bbcf60575ec8ccb36db91b6
Zboniek <karollos.bozek@gmail.com>
2024-12-11 13:00:00 +0100

    another services added

--- src/app/gatunek/gatunek/gatunek.component.ts
+++ src/app/gatunek/gatunek/gatunek.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { GatunekService } from '../../services/gatunek.service';
+import { UserService } from '../../services/users.service'; // Importowanie UserService
 import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-gatunek',
   standalone: true,
@@ -9,13 +11,23 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./gatunek.component.scss'],
 })
 export class GatunekComponent {
-  gatunki: any[] = [];
+  gatunki: any[] = []; // Przechowywanie danych o gatunkach
+  users: any[] = []; // Przechowywanie danych o użytkownikach
 
-  constructor(private gatunekService: GatunekService) {}
+  constructor(
+    private gatunekService: GatunekService, // Wstrzyknięcie serwisu Gatunek
+    private userService: UserService // Wstrzyknięcie serwisu User
+  ) {}
 
   ngOnInit(): void {
+    // Pobieranie danych o gatunkach
     this.gatunekService.getGatuneks().subscribe((data) => {
       this.gatunki = data;
+    });
+
+    // Pobieranie danych o użytkownikach
+    this.userService.getUsers().subscribe((data) => {
+      this.users = data;
     });
   }
 }
---
+++ src/app/services/users.service.ts
@@ -0,0 +1,38 @@
+// user.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private apiUrl = 'https://localhost:7272/api/Uzytkowniks'; // URL do API backendu
+
+  constructor(private http: HttpClient) {}
+
+  // Pobieranie listy użytkowników
+  getUsers(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  // Pobieranie konkretnego użytkownika
+  getUser(id: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${id}`);
+  }
+
+  // Dodawanie nowego użytkownika
+  addUser(user: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, user);
+  }
+
+  // Aktualizacja użytkownika
+  updateUser(id: number, user: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/${id}`, user);
+  }
+
+  // Usuwanie użytkownika
+  deleteUser(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/${id}`);
+  }
+}
